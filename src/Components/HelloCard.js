eff435cf5fb0d78e8785a784978b2c0558384035
Milton <A01634505@itesm.mx>
2021-01-31 19:50:15 -0600

    Added highlights

--- src/Components/HelloCard.js
+++ src/Components/HelloCard.js
@@ -6,12 +6,15 @@ function HelloCard(props) {
             <h2 className="hi-title">Hello, im Milton Eduardo</h2>
             <hr />
             <p className="hi-p">
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-                adipisci velit…
+                Im a <span className="highlight">Computer Systems Engineering</span> student with a
+                passion for continuous learning and making my ideas a{' '}
+                <span className="highlight">reality</span>.
                 <br />
+                <br />I find <span className="highlight">data science</span> really interesting and
+                like getting involved into <span className="highlight">game dev</span> on my free
+                time.
                 <br />
-                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-                adipisci velit...
+                <br />I love coding, videogames and <span className="highlight">penguins 🐧</span>
             </p>
             <span className="hi-grid">{props.children}</span>
         </div>
